4842dbd8c7f5270ad1d6e896f1415807b9f6eeec
Octerry <taaniyu@gmail.com>
2026-01-10 18:42:34 +0100

    Ajout des interractions (sommaire et faire un don)

---
+++ function.js
@@ -0,0 +1,41 @@
+//ON RECUPERE LES ELEMENTS NECESSAIRES
+let buttonToPresentation = document.getElementById('goto_presentation');
+let buttonToRules = document.getElementById('goto_rules');
+let buttonToCrowdfunding = document.getElementById('inner3');
+let buttonToThanks = document.getElementById('goto_thanks');
+let buttonToCredits = document.getElementById('goto_credits');
+
+let presentationRepresentation = document.getElementById('section_presentation');
+let crowdfundingRepresentation = document.getElementById('section_footer')
+let thanksRepresentation = document.getElementById('title')
+let creditsRepresentation = document.getElementById('credits_scrollref')
+
+
+//BOUTONS DU SOMMAIRE REACTIF
+function goTo (scrollValueGoal) {
+    window.scrollTo({
+        top: scrollValueGoal,
+        behavior: "smooth"
+    })
+}
+
+buttonToPresentation.addEventListener('click', function() {
+    goTo(presentationRepresentation.offsetTop)
+});
+
+buttonToRules.addEventListener('click', function() {
+    goTo(presentationRepresentation.offsetTop)
+});
+
+buttonToCrowdfunding.addEventListener('click', function() {
+    goTo(crowdfundingRepresentation.offsetTop)
+})
+
+buttonToThanks.addEventListener('click', function() {
+    goTo(thanksRepresentation.offsetTop)
+})
+
+buttonToCredits.addEventListener('click', function() {
+    goTo(credits_scrollref.offsetTop)
+})
+
